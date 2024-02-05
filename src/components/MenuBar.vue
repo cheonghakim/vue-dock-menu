b31202395b22b179133829f69d2df3a6d5ae53cd
cheonghakim <cheongha.kim@cyberone.kr>
2024-02-05 14:10:08 +0900

    update: css class

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,6 +1,5 @@
 <template>
-  <div
-ref="menuBarRef" :class="[dockClass, 'menu-bar-container', expandClass]" :draggable="draggable" tabindex="0"
+  <div ref="menuBarRef" :class="[dockClass, 'menu-bar-container', expandClass]" :draggable="draggable" tabindex="0"
     :style="menuBarStyle" :showIcon="showIcon" @dragover="handleDragMove" @dragstart="handleDragStart"
     @dragend="handleDragEnd" @touchstart="handleDragStart" @touchmove="handleDragMove" @touchend="handleDragEnd">
     <header>
@@ -9,8 +8,7 @@ ref="menuBarRef" :class="[dockClass, 'menu-bar-container', expandClass]" :dragga
 
     <ul :class="[dockClass, 'menu-bar-items']" draggable="true" @dragstart="handleDragCancel">
       <li v-for="item of menuItems" :key="item.id" :class="[dockClass, 'v-dock-menu-bar-item-wrapper']">
-        <menu-bar-item
-:id="item.id" :dock="dockPosition" :menu-active="menuActive"
+        <menu-bar-item :id="item.id" :dock="dockPosition" :menu-active="menuActive"
           :menu-bar-dimensions="{ height: barHeight, width: barWidth }" :menu="item.menu" :name="item.name"
           :icon="item.icon" :menu-bar-active="menuBarActive" :show-menu="item.showMenu" :theme="theme"
           :is-touch-device="isMobileDevice" :on-selected="handleSelected" :highlight-first-element="highlightFirstElement"
@@ -304,7 +302,7 @@ export default defineComponent({
         dockPosition.value === DockPosition.LEFT ||
         dockPosition.value === DockPosition.RIGHT
       ) {
-        return menuBarActive.value ? "expanded" : "not-expanded";
+        return menuBarActive.value ? "expanded d-flex flex-column" : "not-expanded";
       } else {
         return "";
       }
